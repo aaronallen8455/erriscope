9410d6fcb9531dab40dff49abbf64d09730acb01
Aaron Allen <aaron@flipstone.com>
2022-02-17 15:57:46 -0600

    support 8.10

--- erriscope/src/Erriscope.hs
+++ erriscope/src/Erriscope.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE LambdaCase #-}
 module Erriscope
   ( plugin
@@ -46,9 +47,15 @@ driverPlugin _opts dynFlags = do
 
   putStrLn "Driver plugin"
   pure dynFlags
+#if MIN_VERSION_ghc(9,0,0)
     { Ghc.log_action = \dflags warnReason severity srcSpan msgDoc -> do
-        reportError dflags warnReason severity srcSpan msgDoc
+        reportError dflags severity srcSpan msgDoc
         Ghc.log_action dynFlags dflags warnReason severity srcSpan msgDoc
+#else
+    { Ghc.log_action = \dflags warnReason severity srcSpan pprStyle msgDoc -> do
+        reportError dflags severity srcSpan msgDoc
+        Ghc.log_action dynFlags dflags warnReason severity srcSpan pprStyle msgDoc
+#endif
     , Ghc.hooks = (Ghc.hooks dynFlags)
         { Ghc.runPhaseHook = Just $ \phase filePath dflags -> do
             case phase of
@@ -131,9 +138,9 @@ parsedResult modSum parsedMod
   | otherwise = pure parsedMod
 
 -- | Send error message to server
-reportError :: Ghc.LogAction
-reportError dynFlags _warnReason severity srcSpan msgDoc
-  | Ghc.RealSrcSpan rss _ <- srcSpan
+reportError :: Ghc.DynFlags -> Ghc.Severity -> Ghc.SrcSpan -> Ghc.SDoc -> IO ()
+reportError dynFlags severity srcSpan msgDoc
+  | Ghc.RealSrcSpan' rss <- srcSpan
   , Just errType <- getErrorType severity
   = do
     let modFile = Ghc.bytesFS $ Ghc.srcSpanFile rss
@@ -195,94 +202,3 @@ deleteAll = do
   withMVar connMVar $ \mConn -> for_ mConn $ \conn ->
     WS.sendBinaryData conn (ET.encodeEnvelope msg)
 
--- since the log action deals with one error at a time, add them to a queue and
--- flush on some other compilation stage, although don't think such a phase
--- exists, they will all be per module.
--- type LogAction = DynFlags
---               -> WarnReason
---               -> Severity
---               -> SrcSpan
---               -> MsgDoc
---               -> IO ()
--- 
--- Can use a heuristic: we can know somehow when a module begins compilation
--- (using the parsing phase?) Can use the hscFrontendHook for this.
---
--- Here's what we know for certain:
--- - The module identifier if an error occurs during parsing.
--- - Module name when parsing completes
--- - When type checking completes
--- - We know when a new module compilation starts although not which module.
---
--- So when a module compilation starts, there should be some forthcoming signal.
--- Once such a signal is received, start a timer.
--- If another module begins compiling then stop the timer.
--- If timer expires then emit the errors to the server
--- Don't care about parsing completion, only when errors get emitted and when
--- type checking completes. (errors should all be emitted around the same time?)
--- ALSO want to hold onto warnings and clear them when an event occurs for a
--- given module. Do this when parsing completes for a module.
---
--- Why not send the errors one by one and then have a different message that
--- clears the errors for a given module (triggered by type check completion).
--- Parsing completion would trigger clearing the warnings for a given module.
--- This will change the way the data must be modeled on the server and is also
--- a problem for sending it to the frontend which expects a single chunk of
--- html. Instead, each error would be sent over as an individual chunk of html
--- that must be appended to the sidebar. Clearing the errors for a module would
--- entail doing a complete replace of the sidebar html.
---
--- Also need to clear the ERRORS when parsing completes because there may be
--- old errors that got fixed and we want to remove them so they don't persist.
--- In fact, as soon as either a parse error occurs or parsing completes, clear
--- the errors - but this isn't good enough because there could be multiple
--- parse errors and we don't want one to clear out the other. So back to some
--- timer based heuristic? Can you have more than one parse error for the same
--- module? seems like it would halt at that point?
--- Yes, seems like it's just one parse error at a time.
---
--- Need to check for files being deleted. How should this happen? The simple
--- solution to check for the existence of each known file during plugin
--- initilization. This would occur before compiling each module, which is wasteful
--- but the only other option is to have some heuristic on how much time has passed
--- since errors were added or removed.
---
--- What about this: When error is received for a known file, clear out all the
--- errors (so that there no other known files). The trick is that when a second
--- error is received for that file, we don't want to clear it again. This can
--- be achieved by checking if the same file for which the last error was received.
--- This breaks down in the case where errors for multiple files are emitted
--- concurrently, which can happen when compiling with -j. Do errors get interleaved
--- when compiling concurrently? I'm thinking they don't... This method doesn't
--- work anyways - new files can be added in such a way that the clearing would
--- happen in a poor way. Not necessarily, if we only clear the files that were
--- not compiled since the last time the known file was compiled then that should
--- be OK. This is all predicated on errors not being emitted concurrently - seems
--- like it could happen... Can just check if there are existing errors for that
--- file because existing errors would have been cleared out during the parsing
--- phase so if there is 1 or more error then don't clear other files.
--- Could use the file clear occuring at completion of parse phase or emission
--- of parse phase as the signal to clear other files.
---
--- Is there a way to tell that an error occurred during parsing? Could simply
--- check the error body for certain characteristics. Best option seems to be
--- inspecting the error message to see if it's a parse error.
---
--- So heres the outline of what can happen
---
--- - Error occurs during parsing: Check if the error is a parse failure. If
--- so, clear existing errors for that module then emit the new one.
--- - Module completes the parse phase: Clear existing errors for this module
--- - Error occurs during type checking: Check if error is a parse failure.
--- If not, don't clear errors for this module and emit the new error.
--- - Type checking completes: Clear errors for this module.
---
--- Now to take care of files being deleted:
--- Brute force option is to check for files that no longer exist at each plugin
--- initialization (start of a module compilation). This may be the only truly
--- reliable option given the complications arising from parallel compilation.
--- Since there aren't likely to be a huge number of files with errors, it seems
--- reasonable to prune deleted files each time the plugin initialization occurs.
---
--- When socket initializes, send a message to clear any existing errors on the
--- server.
--- erriscope/src/Erriscope/Internal/GhcFacade.hs
+++ erriscope/src/Erriscope/Internal/GhcFacade.hs
@@ -1,7 +1,11 @@
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE CPP #-}
 module Erriscope.Internal.GhcFacade
   ( module Ghc
+  , pattern RealSrcSpan'
   ) where
 
+#if MIN_VERSION_ghc(9,0,0)
 import GHC.Data.FastString as Ghc
 import GHC.Driver.Hooks as Ghc
 import GHC.Driver.Phases as Ghc
@@ -15,3 +19,24 @@ import GHC.Types.SrcLoc as Ghc
 import GHC.Utils.Error as Ghc
 import GHC.Utils.Outputable as Ghc
 import GHC.Unit.Module as Ghc
+#else
+import FastString as Ghc
+import Hooks as Ghc
+import Plugins as Ghc
+import ErrUtils as Ghc
+import SrcLoc as Ghc
+import Outputable as Ghc
+import DynFlags as Ghc
+import HscTypes as Ghc
+import GHC.Hs as Ghc
+import Module as Ghc
+import DriverPipeline as Ghc
+import DriverPhases as Ghc hiding (Hsc)
+#endif
+
+pattern RealSrcSpan' :: RealSrcSpan -> SrcSpan
+#if MIN_VERSION_ghc(9,0,0)
+pattern RealSrcSpan' loc <- RealSrcSpan loc _
+#else
+pattern RealSrcSpan' loc = RealSrcSpan loc
+#endif
